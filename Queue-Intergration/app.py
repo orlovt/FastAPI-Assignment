50d408e9dfbaa6df5b92a74bbc0995ad97bddd8b
Timothy <orlovt7@yandex.ru>
2024-05-21 16:08:58 -0500

    Added Deployment, which was tested with a locally running postgres image

--- Queue-Intergration/app.py
+++ Queue-Intergration/app.py
@@ -1,6 +1,6 @@
 from aws_cdk import App
 from queue_intergration.queue_intergration_stack import QueueIntergrationStack
-
+print("Creating app...")
 app = App()
 QueueIntergrationStack(app, "QueueIntergrationStack")
 app.synth()
